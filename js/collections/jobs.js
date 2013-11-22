44083f71c62e83d79c4d68c03d0d97a652b4f99e
Patrick <costanzo.patrick@gmail.com>
2013-11-21 21:25:35 -0800

    resolve sorting issue, fixes #1

--- js/collections/jobs.js
+++ js/collections/jobs.js
@@ -1,4 +1,18 @@
 APP.JobCollection = Backbone.Collection.extend({
     model: APP.JobModel,
-    url: "/jobs"
+    url: "/jobs",
+
+    // filter : function(arg) {
+    //     console.log(this);
+
+        // var test = _.filter(this, function(item){
+        //     return item.get("isActive") === true;
+        // });
+
+    //     console.log(test);
+
+    //     return this.filter(function(item){
+    //         return item.get('isActive');
+    //     });
+    // }
 });
--- js/views/masterView.js
+++ js/views/masterView.js
@@ -2,7 +2,7 @@ APP.MasterView = Backbone.View.extend({
     tagName: "div",
     className: "masterView",
 
-    template: _.template('<input type="radio" name="tab" value="all" checked> All   <input type="radio" name="tab" value="applied"> Active<div id="masterView" style="width: 100%;height: 800px; background: #EEEEEE; border: 1px solid #C2C2C2"><div id="tabs"></div><div id="collection"></div></div>'),
+    template: _.template('<input type="text"id="searchbar"/>   <input type="radio" name="tab" value="all" checked> All   <input type="radio" name="tab" value="applied"> Active<div id="masterView" style="width: 100%;height: 800px; background: #EEEEEE; border: 1px solid #C2C2C2"><div id="tabs"></div><div id="collection"></div></div>'),
 
     events: {
         "change input[name='tab']" : "filterCollection"
@@ -29,13 +29,11 @@ APP.MasterView = Backbone.View.extend({
     },
 
     buildCollection: function(collection){
-        
         collection.fetch({
             success: function(){
                 this.joblist = new APP.JobCollectionView({
-                    collection: APP.jobcollection
+                    collection: collection
                 });
-
                 this.joblist.render();
                 $('#collection').html(this.joblist.$el);
             }
@@ -45,11 +43,38 @@ APP.MasterView = Backbone.View.extend({
     filterCollection: function() {
         console.log("EVENT:MasterView Filtered Collection");
         console.log(APP.jobcollection);
-        var newCollection = _.filter(APP.jobcollection, function(item){
+
+        var filtered = APP.jobcollection.filter(function(item){
             return item.get("isActive") === true;
         });
+
+        var newCollection = new APP.JobCollection(filtered);
         console.log(newCollection);
-        console.log(APP.jobcollection);
-        this.buildCollection(newCollection);
+
+        // Wow...okay, so for whatever reason you can't use underscore
+        // functionality by calling _.function(collection, iterator(){})
+        // that is seriously lame. See above for correct way.
+        //
+        // var newCollection = _.filter(APP.jobcollection, function(item){
+        //     console.log(item);
+        //     return item.get("isActive") === true;
+        // });
+        
+
+        // This isn't working how I had hoped, it's re-polling
+        // the collection from teh database effectively 
+        // negating any sorting functionality implemented
+        // prior to the server call.
+        // this.buildCollection(newCollection);
+
+        this.joblist = new APP.JobCollectionView({
+            collection: newCollection
+        });
+        this.joblist.render();
+        $('#collection').html(this.joblist.$el);
+
+        // var test = _.filter(this, function(item){
+        //     return item.get("isActive") === true;
+        // });
     }
 });
